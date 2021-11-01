0751021d441538b0dc864c81ebd552f6c020849c
A00218636 <84596063+A00218636@users.noreply.github.com>
2021-10-31 20:19:58 -0700

    Add project files.

---
+++ CalculatorTest.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using System;
+using SharedCalculator;
+namespace Calculator
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+
+        private IWebDriver _webDriver;
+
+        [TestInitialize]
+       public void Startup()
+        {
+            DriverManager manager = new DriverManager();
+            manager.SetUpDriver(new ChromeConfig());
+
+        }
+
+        //[TestMethod]
+        //https://github.com/A000917542/WebApiTestSkeleton
+        [DataTestMethod]
+        [DataRow(1,1,2)]
+        [DataRow(1, 2, 3)]
+        [DataRow(3, 1, 4)]
+        [DataRow(3, 3, 6)]
+        [DataRow(2, 1, 3)]
+        [DataRow(3, 4, 7)]
+        public void CalculatorAddMethodMustAdd(double left, double right, double expected)
+        {
+            double result;
+            result = SharedCalculator.Calculator.Add(left, right);
+            Assert.AreEqual(expected, result);
+            
+        }
+        [DataTestMethod]
+        [DataRow(1, 1, 0)]
+        [DataRow(1, 2, -1)]
+        [DataRow(3, 1, 2)]
+        [DataRow(3, 3, 0)]
+        [DataRow(2, 1, 1)]
+        [DataRow(3, 4, -1)]
+        public void CalculatorAddMethodMustSubtract(double left, double right, double expected)
+        {
+            double result;
+            result = SharedCalculator.Calculator.Subtract(left, right);
+            Assert.AreEqual(expected, result);
+
+        }
+        [DataTestMethod]
+        [DataRow(1, 1, 1)]
+        [DataRow(1, 2, .5)]
+        [DataRow(3, 1, 3)]
+        [DataRow(3, 3, 1)]
+        [DataRow(2, 1, 2)]
+        [DataRow(2, 4, .5)]
+        public void CalculatorAddMethodMustDivide(double left, double right, double expected)
+        {
+            double result;
+            result = SharedCalculator.Calculator.Divide(left, right);
+            Assert.AreEqual(expected, result);
+
+        }
+        [DataTestMethod]
+        [DataRow(1, 1, 1)]
+        [DataRow(1, 2, 2)]
+        [DataRow(3, 1, 3)]
+        [DataRow(3, 3, 9)]
+        [DataRow(2, 1, 2)]
+        [DataRow(3, 4, 12)]
+        public void CalculatorAddMethodMustMultiply(double left, double right, double expected)
+        {
+            double result;
+            result = SharedCalculator.Calculator.Multiply(left, right);
+            Assert.AreEqual(expected, result);
+
+        }
+
+        //[TestMethod]
+        //public void TestMethod2()
+        //{
+        //}
+
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //}
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a9ae267-8e29-474c-8698-40f9c1dddbab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
